f694adbaad6e44817ebcaa275c9310839b339435
fanzhengchen <fanzhengchen@gmail.com>
2016-01-21 19:55:37 +0800

    pick img

--- app/src/main/java/com/hcb/jingle/frg/personal/MyInfoFrg.java
+++ app/src/main/java/com/hcb/jingle/frg/personal/MyInfoFrg.java
@@ -1,19 +1,35 @@
 package com.hcb.jingle.frg.personal;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.hcb.jingle.GlobalConsts;
 import com.hcb.jingle.R;
 import com.hcb.jingle.actlink.NaviRightDecorator;
+import com.hcb.jingle.biz.ActivitySwitcher;
+import com.hcb.jingle.cache.CacheCenter;
 import com.hcb.jingle.dialog.DatePickerDlg;
+import com.hcb.jingle.dialog.ImagePickerDialog;
 import com.hcb.jingle.dialog.PickTextDlg;
+import com.hcb.jingle.frg.PickAvatarFrg;
 import com.hcb.jingle.frg.TitleFragment;
+import com.hcb.jingle.loader.ImageUploader;
 import com.hcb.jingle.util.ColorUtil;
+import com.hcb.jingle.util.ToastUtil;
+import com.hcb.jingle.util.UriUtil;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import butterknife.Bind;
@@ -23,12 +39,18 @@ import butterknife.OnClick;
 /**
  * Created by Administrator on 2016/1/20.
  */
-public class MyInfoFrg extends TitleFragment implements NaviRightDecorator{
+public class MyInfoFrg extends TitleFragment implements NaviRightDecorator {
 
     @Bind(R.id.frg_my_info_birth_date)
     TextView birthday;
     @Bind(R.id.frg_my_info_gender)
     TextView gender;
+    @Bind(R.id.frg_my_info_portrait)
+    ImageView portrait;
+
+    private File photoFile;
+    private String avatarPath;
+    private String destPath;
 
     @Override
     public int getTitleId() {
@@ -40,6 +62,7 @@ public class MyInfoFrg extends TitleFragment implements NaviRightDecorator{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.frg_my_info, container, false);
         ButterKnife.bind(this, rootView);
+        imagePicker();
         return rootView;
     }
 
@@ -76,5 +99,99 @@ public class MyInfoFrg extends TitleFragment implements NaviRightDecorator{
     public void decorRightBtn(TextView btnImage) {
         btnImage.setText(R.string.confirm);
         btnImage.setTextColor(ColorUtil.getColor(R.color.main_pink));
+    }
+
+    @OnClick(R.id.frg_my_info_portrait_layout)
+    public void imagePicker() {
+
+        ImagePickerDialog imagePickerDialog = new ImagePickerDialog();
+        imagePickerDialog.setHandleImage(new ImagePickerDialog.HandleImage() {
+            @Override
+            public void onPick() {
+                ToastUtil.show("onPick");
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                act.startActivityForResult(intent, GlobalConsts.ACTREQ_PICTURE);
+
+            }
+
+            @Override
+            public void onCamera() {
+                ToastUtil.show("onCamera");
+                photoFile = CacheCenter.genCameraFile(act, genTempCameraFile());
+                if (null == photoFile) {
+                    ToastUtil.show("不能拍照：存储卡空间不足！");
+                    return;
+                }
+                Intent intent = new Intent();
+                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                act.startActivityForResult(intent, GlobalConsts.ACTREQ_CAMERA);
+
+            }
+        });
+        imagePickerDialog.show(getFragmentManager(), "imagePickerDialog");
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == Activity.RESULT_OK) {
+            String path = null;
+            switch (requestCode) {
+                case GlobalConsts.ACTREQ_PICTURE:
+                    Uri uri = data.getData();
+                    path = UriUtil.getPath(act, uri);
+                    break;
+                case GlobalConsts.ACTREQ_CAMERA:
+                    path = photoFile.getAbsolutePath();
+                    break;
+                case GlobalConsts.ACTREQ_CLIP_AVATAR:
+                    avatarPath = destPath;
+                    break;
+            }
+            if (null != path) {
+                photoFile = CacheCenter.genCameraFile(act, genTempCameraFile());
+                if (null == photoFile) {
+                    ToastUtil.show("不能拍照：存储卡空间不足！");
+                    return;
+                }
+                destPath = photoFile.getAbsolutePath();
+                final Bundle bundle = new Bundle();
+                bundle.putString(PickAvatarFrg.SRC, path);
+                bundle.putString(PickAvatarFrg.DST, destPath);
+                ActivitySwitcher.startFrgForResult(act, PickAvatarFrg.class,
+                        bundle, GlobalConsts.ACTREQ_CLIP_AVATAR);
+            } else if (null != avatarPath) {
+                portrait.setImageBitmap(BitmapFactory.decodeFile(avatarPath));
+                if (null != avatarPath) {
+                    uploadImage(avatarPath);
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void uploadImage(String path) {
+
+        showProgressDialog("修改资料", "上传头像...");
+        new ImageUploader().upload(path, new ImageUploader.UploadReactor() {
+            @Override
+            public void onResult(String url) {
+                dismissDialog();
+                if (TextUtils.isEmpty(url)) {
+                    ToastUtil.show(R.string.load_err_retry);
+                } else {
+                    ToastUtil.show("修改成功");
+
+                }
+            }
+        });
+
+    }
+
+    private String genTempCameraFile() {
+        return "camera_" + System.currentTimeMillis() + ".jpg";
     }
 }
